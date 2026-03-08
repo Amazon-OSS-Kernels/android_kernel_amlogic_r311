9b2745565a5da73a7391064327d834acd7518f9f
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 12:47:16 -0500

    AmazonFireTVEditionFHD-6.7.0.8-20241113

--- build_kernel_config.sh
+++ build_kernel_config.sh
@@ -2,21 +2,22 @@
 #
 #  build_kernel_config.sh
 #
-#  Copyright (c) 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#  Copyright (c) 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 ################################################################################
 
-KERNEL_SUBPATH="kernel/amlogic/t950x4/4.9"
-DEFCONFIG_NAME="almond_defconfig"
-TARGET_ARCH="arm"
+KERNEL_SUBPATH="kernel/amlogic/r311"
+DEFCONFIG_NAME="blanche_defconfig"
+TARGET_ARCH="arm64"
 MAKE_DTBS=y
+PARALLEL_EXECUTION="-j8"
 
 # Expected image files are seperated with ":"
-KERNEL_IMAGES="arch/arm/boot/Image:arch/arm/boot/zImage"
+KERNEL_IMAGES="arch/arm64/boot/Image:arch/arm64/boot/Image.gz:arch/arm64/boot/Image.lzo"
 
 ################################################################################
-# NOTE: You must fill in the following with the path to a copy of
-#       a gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf compiler
+# NOTE: You must fill in the following with the path to a copy of an
+# aarch64-linux-gnu compiler, i.e gcc-linaro-aarch64-linux-gnu-4.9-2014.09_linux
 ################################################################################
 CROSS_COMPILER_PATH=""
-TOOLCHAIN_PREFIX="arm-linux-gnueabihf-"
+TOOLCHAIN_PREFIX="aarch64-linux-gnu-"
